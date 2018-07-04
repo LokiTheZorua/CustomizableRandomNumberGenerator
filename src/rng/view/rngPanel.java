ccbd8e92ca494ab6f196d8ea2d15895fbd9f237c
LokiTheZorua <ajones9641@gmail.com>
2018-07-04 15:20:00 -0600

    added panel elements

--- src/rng/view/rngPanel.java
+++ src/rng/view/rngPanel.java
@@ -9,11 +9,49 @@ import java.awt.event.ActionEvent;
 public class rngPanel extends JPanel
 {
 	private rngController appController;
+	private SpringLayout appLayout;
+	private JButton generateButton;
+	private JTextField minField;
+	private JTextField maxField;
+	private JCheckBox decimalBox;
+	private JTextArea numberPresenter;
 	
 	public rngPanel(rngController appController)
 	{
 		super();
 		
 		this.appController = appController;
+		appLayout = new SpringLayout();
+		
+		generateButton = new JButton("Generate");
+		
+		minField = new JTextField();
+		maxField = new JTextField();
+		decimalBox = new JCheckBox();
+		numberPresenter = new JTextArea();
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		this.setLayout(appLayout);
+		this.setBackground(Color.LIGHT_GRAY);
+		this.add(generateButton);
+		this.add(minField);
+		this.add(maxField);
+		this.add(decimalBox);
+		this.add(numberPresenter);
+	}
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
 	}
 }
